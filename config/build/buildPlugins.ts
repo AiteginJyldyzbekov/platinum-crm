b9476c4c618e7702c55a38add63aee800b0401d6
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-11-18 20:54:31 +0600

    feat: hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }

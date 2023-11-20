a6d88408202ccc4f629f632a6dc48f2c20df5f00
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-11-21 03:28:17 +0600

    feat:analyze bundle

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,11 +2,9 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export function buildPlugins ({
-  paths,
-  isDev
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
       template: paths.html
@@ -19,6 +17,9 @@ export function buildPlugins ({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ]
 }

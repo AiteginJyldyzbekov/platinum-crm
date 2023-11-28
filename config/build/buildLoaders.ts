dbc269fa97cfe49df91609bda696454c9c416630
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-11-28 10:56:41 +0600

    fix:lint errors

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import { type BuildOptions } from './types/config'
 import type webpack from 'webpack'
 import { buildCssLoader } from './loaders/buildCssLoader'
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack']

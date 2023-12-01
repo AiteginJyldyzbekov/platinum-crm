8b655b9fef843d391cd861867d7bea1a8045c6d5
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-12-01 17:39:20 +0600

    feat:added ref

--- loki.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // ...other config goes here
-    fileNameFormatter: ({ configurationName, kind, story, parameters }) =>
-      `${configurationName}/${kind} ${story}`.toLowerCase(),
-  };

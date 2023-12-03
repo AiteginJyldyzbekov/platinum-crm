6f695d5a3e452fbfbfb47306de71954934e18b12
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-12-03 23:42:17 +0600

    fix:lint

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -25,10 +25,10 @@ export default ({ config }: { config: webpack.Configuration }) => {
     use: ['@svgr/webpack']
   })
   config.module.rules.push(buildCssLoader(true))
-  
+
   config.plugins.push(new DefinePlugin(
-    { __IS_DEV__: true },
-));
+    { __IS_DEV__: true }
+  ))
 
   return config
 }

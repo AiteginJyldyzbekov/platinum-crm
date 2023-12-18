31cfdd00c9612ececcee325e92fb602da4176e18
Aitegin <jyldyzbekov06aiti@gmail.com>
2023-12-18 07:03:34 +0600

    fix:husky and eslint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next'
 import styles from './Navbar.module.scss'
 
 export const Navbar = () => {
+  const { t } = useTranslation()
   return (
     <div className={styles.wrapper}>
-      NavBar
+      {t('Navbar')}
     </div>
   )
 }

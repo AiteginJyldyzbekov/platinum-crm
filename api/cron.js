0897dc72300b0f9680ed8c91c23dd5f4b0776d5e
Aitegin <jyldyzbekov06aiti@gmail.com>
2024-01-20 00:53:40 +0600

    feat:changed inteval

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).end('Hello Cron');
+export default function handler() {
+  const currentTime = new Date();
+  const formattedTime = currentTime.toISOString(); // Преобразуем в строку в формате ISO
+
+  // Выводим запись в консоль с текущим временем
+  console.log(`Cron job executed at: ${formattedTime}`);
+
+  // Возвращаем сообщение в виде строки
+  return `Cron job executed at: ${formattedTime}`;
 }

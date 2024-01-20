ee7efb1583aaee736cabf2c91f5085063d52e910
Aitegin <jyldyzbekov06aiti@gmail.com>
2024-01-21 04:08:42 +0600

    fix:cron

--- api/cron/index.js
+++ api/cron/index.js
@@ -1,4 +1,14 @@
+import { addDoc } from "firebase/firestore"
+import { carsRef } from "shared/config/firebase/firebase"
+
 export async function GET() {
+    const newCar = {
+        car: 'CRON',
+        model: 'CRON',
+        color: 'CRON',
+        numberPlate: 'CRON'
+    }
+    addDoc(carsRef, newCar)
     return new Response("Hello from cron", {
         status: 200
     })
